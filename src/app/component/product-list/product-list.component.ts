cb4d7e13405535a9b27e541f38074faf1663439b
Roshan Kumar <rsahu7989@gmail.com>
2024-12-07 20:09:15 +0000

    add product sorting and searching

--- src/app/component/product-list/product-list.component.ts
+++ src/app/component/product-list/product-list.component.ts
@@ -7,6 +7,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 @Component({
   selector: 'app-product-list',
   imports: [
@@ -15,20 +17,55 @@ import { RouterModule } from '@angular/router';
     MatCardModule, // material card
     MatButtonModule, // material button
     MatGridListModule, //material grid
-    MatIconModule // to usematerial icons
+    MatIconModule, // to usematerial icons
+    MatInputModule,
+    MatSelectModule
+
   ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  filteredProduct: Product[] = [];
+  sortOrder: String = "";
+
   constructor(private productService: ProductService) {
 
   }
   ngOnInit(): void {
+
     this.productService.getProducts()
       .subscribe(data => {
         this.products = data;
+        this.filteredProduct = data;
       })
+  }
+
+  filterProducts(e: Event) {
+    let searchTerm = (e.target as HTMLInputElement).value;
+    searchTerm = searchTerm.toLowerCase();
+    if (searchTerm === "") {
+      this.filteredProduct = this.products;
+    }
+    else {
+      this.filteredProduct = this.products.filter(product => {
+        return product.name.toLowerCase().includes(searchTerm);
+      })
+      this.sortProducts(this.sortOrder);
+
+      console.log(this.filteredProduct);
+    }
+
+  }
+
+  sortProducts(sortValue: String) {
+    this.sortOrder = sortValue;
+    if (this.sortOrder === "priceLowHigh") {
+      this.filteredProduct.sort((a, b) => a.price - b.price)
+    }
+    else if (this.sortOrder === "priceHighLow") {
+      this.filteredProduct.sort((a, b) => b.price - a.price)
+    }
   }
 }
